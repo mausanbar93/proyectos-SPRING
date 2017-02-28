38db73f75018dba14279f514c2e46cf4a2aba203
Mauricio Sanchez Barragan <mauricio.sanchez@seratic.com>
2017-02-27 23:14:56 -0500

    Agregando modelos (Cliente, Usuario, Orden, entre otros)
    Se agregan los modelos entidad relación (Cliente->Customer,
    Usuario->User, Orden->Orden, Envio->Shipping, Facturacion->Billing,
    entre otros)

--- eMusicStore/src/main/java/com/emusicstore/model/Cart.java
+++ eMusicStore/src/main/java/com/emusicstore/model/Cart.java
@@ -1,22 +1,74 @@
 package com.emusicstore.model;
 
+import javax.persistence.*;
+import java.io.Serializable;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * Created by Administrador on 23/02/2017.
  */
-public class Cart {
-    private String cartId;
-    private Map<String, CartItem> cartItems;
+@Entity
+public class Cart implements Serializable {
+
+    private static final long serialVersionUID = 877479653780293877L;
+
+    @Id
+    @GeneratedValue
+    private int cartId;
+
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<CartItem> cartItems;
+
+    @OneToOne
+    @JoinColumn(name = "customerId")
+    private Customer customer;
+
     private double granTotal;
+
+    public int getCartId() {
+        return cartId;
+    }
+
+    public void setCartId(int cartId) {
+        this.cartId = cartId;
+    }
+
+    public List<CartItem> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<CartItem> cartItems) {
+        this.cartItems = cartItems;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public double getGranTotal() {
+        return granTotal;
+    }
+
+    public void setGranTotal(double granTotal) {
+        this.granTotal = granTotal;
+    }
+
+    /*   private String cartId;
+    private Map<String, CartItem> cartItems;
+    private double granTotal;*/
 
     /**
      * Método que permite agregar objetos al carro de compras
      * @param item Objeto o árticulo a comprar
      * @result cartItems Listado actualizado de objetos listados en el carro de compras
      * */
-    public void addCartItem(CartItem item){
+    /*public void addCartItem(CartItem item){
         String productId = item.getProduct().getProductId();
 
         if(cartItems.containsKey(productId)){
@@ -28,23 +80,23 @@ public class Cart {
         }
         updateGranTotal();
     }
-
+*/
     /**
      * Método que permite eliminar objetos del carro de compras
      * @param item Objeto o árticulo a eliminar
      * @result cartItems Listado actualizado de objetos listados en el carro de compras
      * */
-    public void removeCartItem(CartItem item){
+    /*public void removeCartItem(CartItem item){
         String productId = item.getProduct().getProductId();
         cartItems.remove(productId);
         updateGranTotal();
-    }
+    }*/
 
     /**
      * Método que permite actualizar el listado de objetos en el carro de compras
      * @result granTotal Valor total de precios en el listado del carro de compras
      * */
-    public void updateGranTotal(){
+    /*public void updateGranTotal(){
         granTotal = 0;
         for(CartItem item : cartItems.values()){
             //granTotal = granTotal + item.getTotalPrice();
@@ -84,5 +136,5 @@ public class Cart {
 
     public void setGranTotal(double granTotal) {
         this.granTotal = granTotal;
-    }
+    }*/
 }
